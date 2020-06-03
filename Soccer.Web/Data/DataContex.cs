69e8919e32c1ac9c249099b20daec1d22272b349
vvw2099 <vvw2099@hotmail.com>
2020-06-02 22:46:31 -0600

    AddDatabase

---
+++ Soccer.Web/Data/DataContex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data.Entities;
+
+namespace Soccer.Web.Data
+{
+    public class DataContex: DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options):base(options)
+        {
+
+        }
+
+        public DbSet<GroupDetailEntity> GroupDetails { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+
+        public DbSet<MatchEntity> Matches { get; set; }
+
+        
+        public DbSet<TeamEntity> Teams { get; set; }
+
+        public DbSet<TournamentEntity> Tournaments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TeamEntity>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
+    }
+}
---
+++ Soccer.Web/Data/Entities/TeamEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.Web.Data.Entities
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {0} can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Logo")]
+        public string LogoPath { get; set; }
+    }
+}
